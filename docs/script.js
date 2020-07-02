34814bc6edcedecbf484c85ffb54132b03c5b882
clbokea <clbo@kea.dk>
2020-07-02 10:33:38 +0200

    js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 $().ready(function() {
 	$("h1:first").css("display", "none");
-	$(".markdown-body").prepend("<div>This is added from script file</div>")
+	$(".markdown-body").prepend("<div><i>Dat19b, Programmering 2, 3. Semester Forår 2020, Underviser: Claus Bove, clbo@kea.dk</i></div>")
 });
 
 
