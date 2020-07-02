2771d80eaf57b7c6fee5d3d8b65d6ac036c5567e
clbokea <clbo@kea.dk>
2020-07-02 10:38:26 +0200

    js

--- docs/script.js
+++ docs/script.js
@@ -1,6 +1,6 @@
 $().ready(function() {
 	$("h1:first").css("display", "none");
-	$(".markdown-body").prepend("<div><i>Dat19b, Programmering 2, 3. Semester Forår 2020, Underviser: Claus Bove, clbo@kea.dk</i></div>")
+	$(".markdown-body").prepend("<div><i><small>Dat19b, Programmering 2, 3. Semester Forår 2020, Underviser: Claus Bove, clbo@kea.dk</small></i></div>")
 });
 
 
